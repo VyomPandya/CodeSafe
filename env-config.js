a6b5ce3ff97209346b44949a3effff769e16cdc2
VyomPandya <126187252+VyomPandya@users.noreply.github.com>
2025-03-27 16:33:46 +0000

    Deploying to gh-pages from @ VyomPandya/CodeSafe@a8575b35113e728b7e6034ea5e5999a6c543d6eb 🚀

---
+++ env-config.js
@@ -0,0 +1,5 @@
+// This file is auto-generated during deployment
+window._env_ = window._env_ || {};
+window._env_.SUPABASE_URL = "";
+window._env_.SUPABASE_ANON_KEY = "";
+window._env_.OPENROUTER_API_KEY = "";
